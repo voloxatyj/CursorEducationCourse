d3b3789e051fd8c4452cac3f9c850e7d7911a6e8
MedynskyyOstap <47416022+voloxatyj@users.noreply.github.com>
2020-08-21 11:11:21 +0300

    after @liubomyrkapov remarks

--- 08-hw-Prototype&&Class/index.js
+++ 08-hw-Prototype&&Class/index.js
@@ -1,39 +1,3 @@
-class Student {
-	constructor (university, course, fullName, isStudent) {
-		this.university = university, 
-		this.course = course, 
-		this.fullName = fullName,
-		this._marks = [5, 4, 4, 5],
-		this.isStudent = isStudent
-	}
-	getInfo() {
-		if(this.isStudent) {
-			studentInfo.innerHTML = 
-			`<div class="m-4">
-				<h3>University</h3><h5>${this.university}</h5>
-				<h3>Course</h3><h5>${this.course}</h5>
-				<h3>FullName</h3><h5>${this.fullName}</h5>
-				<h3>Marks</h3><h5>${this._marks}</h5>
-			</div>`
-		} else studentInfo.innerHTML = `<p class="m-4"><strong>student dismiss</strong></p>`
-	}
-	getAverageMark() {
-		return (this._marks.reduce((acc, next) => acc += next) / this._marks.length).toFixed(1)
-	}
-	dismiss() {
-		this.isStudent = false
-	}
-	recover() {
-		this.isStudent = true
-	}
-	get marks() {
-		return this._marks 
-	}
-	set marks(value) {
-		this.marks.push(+value)
-	}
-}
-
 const btnAdd = document.getElementById('add')
 const btnThrow = document.getElementById('dismiss')
 const btnRecover = document.getElementById('recover')
@@ -44,29 +8,67 @@ const btnaddBudgetStudent = document.getElementById('addBudgetStudent')
 const university = document.getElementById('university')
 const course = document.getElementById('course')
 const fullname = document.getElementById('fullname')
-const isStudent = document.getElementById('isStudent')
 const studentInfo = document.getElementById('studentInfo')
 const budgetStudentInfo = document.getElementById('budgetStudentInfo')
 const rate = document.getElementById('rate')
-let student = null
 let addBudgetStudent = null
+let student = null
+const rates = [5, 4, 4, 5]
+class Student {
+	constructor (university, course, fullName) {
+		this.university = university, 
+		this.course = course, 
+		this.fullName = fullName,
+		this._isStudent = true,
+		this._marks = rates
+	}
+	getInfo() {
+		studentInfo.innerHTML = 
+		`<div class="m-4">
+		<h3>University</h3><h5>${this.university}</h5>
+		<h3>Course</h3><h5>${this.course}</h5>
+		<h3>FullName</h3><h5>${this.fullName}</h5>
+		<h3>Marks</h3><h5>${this._marks}</h5>
+		</div>`
+	}
+	getAverageMark() {
+		return (this._marks.reduce((acc, next) => acc += next) / this._marks.length).toFixed(1)
+	}
+	dismiss() {
+		this.isStudent = false,
+		rates.concat(this._marks)
+		this._marks = null
+	}
+	recover() {
+		this.isStudent = true
+		this._marks = rates
+	}
+	get marks() {
+		return this._marks 
+	}
+	set marks(value) {
+		this.marks.push(+value)
+	}
+	set isStudent(value) {
+		this._isStudent = value
+	}
+}
+
 
 btnAdd.addEventListener('click', function(e){
 	e.preventDefault()
-	student = new Student(university.value, course.value, fullname.value, isStudent.checked ? true : false)
+	student = new Student(university.value, course.value, fullname.value)
 })
 
 btnInfo.addEventListener('click', function(){
-	student.getInfo()
+	student === null ? studentInfo.innerHTML = `<h3 class="m-4">You must add student!!</h3>` : student.getInfo()
 })
 
 btnAverage.addEventListener('click', function(){
-	if(student.isStudent) {
-			studentInfo.innerHTML =
-			`<div class="m-4">
-				<h3>getAverageMark</h3><h5>${student.getAverageMark()}</h5>
-			</div > `
-		} else studentInfo.innerHTML = `< p class="m-4" > <strong>you can’t</strong></ >`
+		studentInfo.innerHTML =`
+		<div class="m-4">
+			<h3>getAverageMark</h3><h5>${student.getAverageMark()}</h5>
+		</div >`
 })
 
 btnRecover.addEventListener('click', function(){
@@ -76,44 +78,37 @@ btnRecover.addEventListener('click', function(){
 
 btnThrow.addEventListener('click', function(){
 	student.dismiss()
-	studentInfo.innerHTML = `<p class="m-4"><strong>student kick off!!<strong></p>`
+	studentInfo.innerHTML = `<h3 class="m-4"><strong>Student kick off!!<strong></h3>`
 })
 
 btnMarks.addEventListener('click', function(){
-	if(student.isStudent){
+	if (student._isStudent){
 		student.marks = rate.value
-		studentInfo.innerHTML =
-			`<div class="m-4">
-				<h3>University</h3><h5>${student.university}</h5>
-				<h3>Course</h3><h5>${student.course}</h5>
-				<h3>FullName</h3><h5>${student.fullName}</h5>
-				<h3>Marks</h3><h5>${student._marks}</h5>
-			</>`
-	} else studentInfo.innerHTML = `<p class="m-4"><strong>student dismiss</strong></p>`
+		student.getInfo()
+	} else studentInfo.innerHTML = `<h3 class="m-4">Please, include student</h3>`
 })
 
 class BudgetStudent extends Student {
-	constructor (university,course,fullName,isStudent, _marks) {
-		super(university, course, fullName,isStudent,_marks)
-		setInterval(() => this.getScholarship(), 30000)
+	constructor (university, course, fullName, _marks, isStudent) {
+		super(university, course, fullName, _marks, isStudent)
+		setInterval(() => {
+			if (budgetStudent._isStudent && super.getAverageMark() > 4) {
+				console.log('Ви отримаєте 1400 грн. стипендії')
+			} else {
+				console.log(`Студент не отримає стипендії`)
+			}
+		}, 30000)
 		this.university = university,
 		this.course = course,
 		this.fullName = fullName,
-		this.isStudent = isStudent
-		this._marks = _marks
-	}
-	getScholarship() {
-		if (this.isStudent && super.getAverageMark()>4) {
-			console.log('Ви отримаєте 1400 грн. стипендії')
-		} else  {
-			console.log(`Студент не отримає стипендії`)
-		}
+		this._marks = _marks,
+		this._isStudent = isStudent
 	}
 }
 
 btnaddBudgetStudent.addEventListener('click', () => {
-	if(student.isStudent) {
-		budgetStudent = new BudgetStudent(student.university, student.course, student.fullName, student.isStudent, student._marks)
+	budgetStudent = new BudgetStudent(student.university, student.course, student.fullName, student._marks, student._isStudent)
+	if (budgetStudent._isStudent) {
 		budgetStudentInfo.innerHTML = `<p><strong>Студента перевели на бюджет</strong></p>` 
 		budgetStudentInfo.style.color = 'green'
 	}  else {
