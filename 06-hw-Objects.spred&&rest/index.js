5be347feb49b815aa945895376ec4b01c23e872c
MedynskyyOstap <47416022+voloxatyj@users.noreply.github.com>
2020-08-30 13:06:54 +0300

    1) getSubjects повинна приймати обєкт студента і повертати список предметів для даного студента. Те ж саме стосується і getAverageMark, getStudentInfo 2) return JSON.stringify(result[0].name) тут можна спростити до return result[0].name
    3) calculateWordLetters має повертати об'єкт

--- 06-hw-Objects.spred&&rest/index.js
+++ 06-hw-Objects.spred&&rest/index.js
@@ -1,3 +1,4 @@
+// const name = prompt(`Введіть ім’я студента, котрий вас цікавить (Anton,Victor,Tanya)`)
 const result = document.getElementById('answers')
 
 /* Arguments */
@@ -27,28 +28,23 @@ const students = [{
 	}
 }];
 
+const student = students.filter(item => item.name === name)
+// student.length === 0 ? alert('Такого студента в списках немає') : -1
+
 function getSubjects (data) {
-	let name = prompt(`Введіть ім’я студента, предмети які він вивчає(Anton,Victor,Tanya)`)
-	let result = data.filter(item => item.name === name)
-	return `[${Object.keys(result[0].subjects).map(item => (item[0].toUpperCase() + item.slice(1).replace('_', ' ')))}]`
+	// return `[${Object.keys(data[0].subjects).map(item => (item[0].toUpperCase() + item.slice(1).replace('_', ' ')))}]`
 }
 
 
-function getAverageMark (data, name=undefined) {
-	let student = ''
-	if(name === undefined){
-		student = prompt(`Введіть ім’я студента, середню оцінку котрого ви хочете взнати(Anton,Victor,Tanya)`)
-	}
-	const infoOfStudent = data.filter(item => item.name === name || item.name === student)  
-	if (typeof infoOfStudent[0].subjects === 'object'){
-		return Number((Object.values(infoOfStudent[0].subjects)
-			.reduce((acc, next) => acc += next.reduce((acc, next) => acc += next, 0) / next.length, 0) / Object.values(infoOfStudent[0].subjects).length).toFixed(2))
-		} else return infoOfStudent[0].subjects
+function getAverageMark (data) {
+	// if (typeof data[0].subjects === 'object'){
+	// 	return Number((Object.values(data[0].subjects)
+	// 		.reduce((acc, next) => acc += next.reduce((acc, next) => acc += next, 0) / next.length, 0) / Object.values(data[0].subjects).length).toFixed(2))
+	// 	} else return data[0].subjects
 }
 
 function getStudentInfo (data) {
-	let name = prompt(`Введіть ім’я студента, інформацію про котрого ви хочете взнати(Anton,Victor,Tanya)`)
-	const studentInfo = data.filter(item => item.name === name).map(item => {
+	const studentInfo = data.map(item => {
 		item.subjects = getAverageMark(data, name)
 		return item
 	})
@@ -67,28 +63,40 @@ function getBestStudent (data) {
 			rate: +res
 		}
 	}).sort((a, b) => b.rate - a.rate)
-	return JSON.stringify(result[0].name)	
+	return result[0].name	
 }
+
+// function calculateWordLetters (word) {
+// 	const test = [...word.replace(' ','')]
+// 	const res = new Map()
+// 	let i = 1
+// 	test.forEach(item => {
+// 		if(res.has(item)) {
+// 			i = res.get(item) + 1
+// 			res.delete(item)
+// 			res.set(item,i)
+// 		} else res.set(item, i)
+// 	})
+// 	return JSON.stringify(Object.fromEntries(res.entries()))
+// }
 
 function calculateWordLetters (word) {
 	const test = [...word.replace(' ','')]
-	const res = new Map()
-	let i = 1
-	test.forEach(item => {
-		if(res.has(item)) {
-			i = res.get(item) + 1
-			res.delete(item)
-			res.set(item,i)
-		} else res.set(item, i)
-	})
-	return JSON.stringify(Object.fromEntries(res.entries()))
+	const check = {}
+	for(let element of test) {
+		if (Object.keys(check).includes(element)) {
+			check[element]++
+		} else check[element] = 1
+	}
+	return JSON.stringify(check)
 }
 
+
 document.writeln(`
-<p>Створіть функцію getSubjects, яка повертає список предметів для конкретного студента.\n ${getSubjects(students)}</p> 
-<p>Створіть функцію getStudentInfo --> яка повертає інформацію загального виду по переданому студенту\n ${getStudentInfo(students)}</p> 
+<p>Створіть функцію getSubjects, яка повертає список предметів для конкретного студента.\n ${getSubjects(student)}</p> 
+<p>Створіть функцію getStudentInfo --> яка повертає інформацію загального виду по переданому студенту\n ${getStudentInfo(student)}</p> 
 <p>Ствроіть функцію getStudentsNames --> яка повертає імена студентів у алфавітному порядку.\n ${getStudentsNames(students)}</p>
 <p>Створіть функцію getBestStudent --> яка повертає кращого студента зі списку по показнику середньої оцінки\n ${getBestStudent(students)}</p>
-<p>Створіть функцію getAverageMark --> яка поверне середню оцінку по усім предметам для переданого студента\n ${getAverageMark(students)}</p> 
+<p>Створіть функцію getAverageMark --> яка поверне середню оцінку по усім предметам для переданого студента\n ${getAverageMark(student)}</p> 
 <p>Створіть функцію calculateWordLetters --> яка повертає обє'кт, в якому ключі це букви у слові, а значення – кількість їх повторень\n ${calculateWordLetters("тест")}</p>  
 `)
