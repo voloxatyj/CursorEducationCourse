d2cf0e023a29cc1414e1634e897bae7018e1b1fe
MedynskyyOstap <47416022+voloxatyj@users.noreply.github.com>
2020-09-03 17:40:32 +0300

    1) getAverageMark, getSubjects пусті функції 2) деякі функції повертають стрічку (JSON.stringify повертає стрічку), хоча по умові мали б повертати обєкт

--- 06-hw-Objects.spred&&rest/index.js
+++ 06-hw-Objects.spred&&rest/index.js
@@ -1,4 +1,4 @@
-// const name = prompt(`Введіть ім’я студента, котрий вас цікавить (Anton,Victor,Tanya)`)
+const name = prompt(`Введіть ім’я студента, котрий вас цікавить (Anton,Victor,Tanya)`)
 const result = document.getElementById('answers')
 
 /* Arguments */
@@ -29,18 +29,18 @@ const students = [{
 }];
 
 const student = students.filter(item => item.name === name)
-// student.length === 0 ? alert('Такого студента в списках немає') : -1
+student.length === 0 ? alert('Такого студента в списках немає') : -1
 
 function getSubjects (data) {
-	// return `[${Object.keys(data[0].subjects).map(item => (item[0].toUpperCase() + item.slice(1).replace('_', ' ')))}]`
+	return `[${Object.keys(data[0].subjects).map(item => (item[0].toUpperCase() + item.slice(1).replace('_', ' ')))}]`
 }
 
 
 function getAverageMark (data) {
-	// if (typeof data[0].subjects === 'object'){
-	// 	return Number((Object.values(data[0].subjects)
-	// 		.reduce((acc, next) => acc += next.reduce((acc, next) => acc += next, 0) / next.length, 0) / Object.values(data[0].subjects).length).toFixed(2))
-	// 	} else return data[0].subjects
+	if (typeof data[0].subjects === 'object'){
+		return Number((Object.values(data[0].subjects)
+		.reduce((acc, next) => acc += next.reduce((acc, next) => acc += next, 0) / next.length, 0) / Object.values(data[0].subjects).length).toFixed(2))
+	} else return data[0].subjects
 }
 
 function getStudentInfo (data) {
@@ -48,7 +48,7 @@ function getStudentInfo (data) {
 		item.subjects = getAverageMark(data, name)
 		return item
 	})
-	return (JSON.stringify(studentInfo[0]))
+	return studentInfo[0]
 }
 
 function getStudentsNames (data) {
@@ -67,36 +67,35 @@ function getBestStudent (data) {
 }
 
 // function calculateWordLetters (word) {
-// 	const test = [...word.replace(' ','')]
-// 	const res = new Map()
-// 	let i = 1
-// 	test.forEach(item => {
-// 		if(res.has(item)) {
-// 			i = res.get(item) + 1
-// 			res.delete(item)
-// 			res.set(item,i)
-// 		} else res.set(item, i)
-// 	})
-// 	return JSON.stringify(Object.fromEntries(res.entries()))
-// }
-
-function calculateWordLetters (word) {
-	const test = [...word.replace(' ','')]
-	const check = {}
-	for(let element of test) {
-		if (Object.keys(check).includes(element)) {
-			check[element]++
-		} else check[element] = 1
-	}
-	return JSON.stringify(check)
-}
-
-
-document.writeln(`
-<p>Створіть функцію getSubjects, яка повертає список предметів для конкретного студента.\n ${getSubjects(student)}</p> 
-<p>Створіть функцію getStudentInfo --> яка повертає інформацію загального виду по переданому студенту\n ${getStudentInfo(student)}</p> 
-<p>Ствроіть функцію getStudentsNames --> яка повертає імена студентів у алфавітному порядку.\n ${getStudentsNames(students)}</p>
-<p>Створіть функцію getBestStudent --> яка повертає кращого студента зі списку по показнику середньої оцінки\n ${getBestStudent(students)}</p>
-<p>Створіть функцію getAverageMark --> яка поверне середню оцінку по усім предметам для переданого студента\n ${getAverageMark(student)}</p> 
-<p>Створіть функцію calculateWordLetters --> яка повертає обє'кт, в якому ключі це букви у слові, а значення – кількість їх повторень\n ${calculateWordLetters("тест")}</p>  
-`)
+	// 	const test = [...word.replace(' ','')]
+	// 	const res = new Map()
+	// 	let i = 1
+	// 	test.forEach(item => {
+		// 		if(res.has(item)) {
+			// 			i = res.get(item) + 1
+			// 			res.delete(item)
+			// 			res.set(item,i)
+			// 		} else res.set(item, i)
+			// 	})
+			// 	return JSON.stringify(Object.fromEntries(res.entries()))
+			// }
+			
+			function calculateWordLetters (word) {
+				const test = [...word.replace(' ','')]
+				const check = {}
+				for(let element of test) {
+					if (Object.keys(check).includes(element)) {
+						check[element]++
+					} else check[element] = 1
+				}
+				return check
+			}
+	
+			document.writeln(`
+			<p>Створіть функцію getSubjects, яка повертає список предметів для конкретного студента.\n ${getSubjects(students)}</p> 
+			<p>Створіть функцію getStudentInfo --> яка повертає інформацію загального виду по переданому студенту\n ${JSON.stringify(getStudentInfo(student))}</p> 
+			<p>Ствроіть функцію getStudentsNames --> яка повертає імена студентів у алфавітному порядку.\n ${getStudentsNames(students)}</p>
+			<p>Створіть функцію getBestStudent --> яка повертає кращого студента зі списку по показнику середньої оцінки\n ${getBestStudent(students)}</p>
+			<p>Створіть функцію getAverageMark --> яка поверне середню оцінку по усім предметам для переданого студента\n ${getAverageMark(student)}</p> 
+			<p>Створіть функцію calculateWordLetters --> яка повертає обє'кт, в якому ключі це букви у слові, а значення – кількість їх повторень\n ${JSON.stringify(calculateWordLetters("тест"))}</p>  
+			`)
