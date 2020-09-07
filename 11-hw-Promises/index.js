6ce3a56754511d5d4e7724e647cf06bd8b9089e0
MedynskyyOstap <47416022+voloxatyj@users.noreply.github.com>
2020-09-07 13:14:41 +0300

    Не ок(( в тебе виводить одинакові символи. Для цього потрібно на один сеттаймаут повертати один символ а потім їх об’єднати. Тоді в тебе точно будуть різні символи

--- 11-hw-Promises/index.js
+++ 11-hw-Promises/index.js
@@ -1,28 +1,24 @@
-function getRandomChinese(i) {
-	let time = 50 * i
+async function getRandomChinese(i) {
 	const date = Date.now().toString()
 	const dateSign = date.slice(date.length-5)
-	const result = new Promise(function(resolve,reject){
-			try {
-				setTimeout(() => resolve(String.fromCharCode(dateSign)), time)
-			} catch (error) {
-				console.error(error)
-			}
-		})
-	return result
-	}
-	
-	
-	(function(){
-		const length = 4
-		let sign = ''
-		let i = 1
-		while(i<=length){
-			let time = 50 * i
-			getRandomChinese(i).then(data => {
-				document.getElementById('text').innerHTML += `<p>Викликавши getRandomChinese, отримаємо результат "${sign += data}" за ${time} ms</p>`
-			})
-			i++
+		try {
+			const result = await String.fromCharCode(dateSign)
+			return result
+		} catch (error) {
+			console.error(error)
 		}
-	}())
+}
+	
+const length = 4
+let sign = ''
+let i = 1
+while(i<=length){
+	let time = 50 * i
+	setTimeout(() => {
+		getRandomChinese(i)
+			.then(data => document.getElementById('text').innerHTML += 
+			`<p>Викликавши getRandomChinese, отримаємо результат "${sign += data}" за ${time} ms</p>`)
+	},time)
+	i++
+}
 
